471f4c88418262d5af578982bf31835f633b915e
abhishekkumar1101 <152615665+abhishekkumar1101@users.noreply.github.com>
2023-12-22 15:59:20 +0530

    Add files via upload

---
+++ test/first.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello world";
+    cout<<endl;
+    cout<<"hello world\n";
+    return 0;
+}
+/*you can also make this program instead of written above
+
+#include<iostream>
+
+int main(){
+   std::cout<<"hello world"<<std::endl;
+    return 0;
+}
+*/
---
+++ test/initialpractice.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=3;
+cout<< a <<endl;
+char b='a';
+cout<< b <<endl;
+float c=4.5;
+cout<< c <<endl;
+bool flag = true;
+cout<< flag<<endl;
+int size =sizeof(b);
+cout<< "the size of integer is :"<< size <<endl;
+
+
+}
